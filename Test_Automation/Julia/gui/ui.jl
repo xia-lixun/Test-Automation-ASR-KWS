5248714bcbc3ce5cedca2449010280efc8a24663
lixun.xia <lixun.xia@yahoo.com>
2018-07-02 17:21:48 +0800

    gui update for better config

--- Test_Automation/Julia/gui/ui.jl
+++ Test_Automation/Julia/gui/ui.jl
@@ -67,94 +67,146 @@ end
 
 
 function ui_tk()
-    w = Toplevel("Automatic Audio Test Tool - CoC Suzhou", 800, 600)
-    pack_stop_propagate(w)
+    w = Tk.Toplevel("Automatic Audio Test Tool - CoC Suzhou",800, 600)
+    Tk.pack_stop_propagate(w)
    
-    # menu
-    m = Menu(w)
-    fmenu = menu_add(m, "File")
-    omenu = menu_add(m, "Options")
-    hmenu = menu_add(m, "Help")
 
-    menu_add(fmenu, "Open File...", (path)->println("open file dialog..."))
+    m = Tk.Menu(w)
+    fmenu = Tk.menu_add(m, "File")
+    omenu = Tk.menu_add(m, "Options")
+    hmenu = Tk.menu_add(m, "Help")
+
+
+    conf = Dict{String, Any}()
+    function loadconf(path)
+        conf = JSON.parsefile(Tk.GetOpenFile())
+    end
+    function saveconf(path)
+        open(Tk.GetSaveFile(), "w") do fid
+            write(fid, JSON.json(conf))
+        end
+    end
+
+    menu_add(fmenu, "Open File...", loadconf)
+    menu_add(fmenu, "Save As...", saveconf)
     menu_add(fmenu, Separator(w))
     menu_add(fmenu, "Close Tool", (path)->destroy(w))
     
-    cb = Checkbutton(w, "something visible")
-    set_value(cb, true)
-    menu_add(omenu, cb)
+
+    cb_ut = Checkbutton(w, "Use Turntable")
+    set_value(cb_ut, true)
+    menu_add(omenu, cb_ut)
+
+    cb_cdis = Checkbutton(w, "Capture DUT Internal Signals")
+    set_value(cb_cdis, true)
+    menu_add(omenu, cb_cdis)
+    
+    cb_dcdc = Checkbutton(w, "DUT Clock Drift Compensation")
+    set_value(cb_dcdc, true)
+    menu_add(omenu, cb_dcdc)
+
     menu_add(omenu, Separator(w))
-    rb = Radio(w, ["option 1", "option 2"])
-    set_value(rb, "option 1")
-    menu_add(omenu, rb)
+    # rb = Radio(w, ["option 1", "option 2"])
+    # set_value(rb, "option 1")
+    # menu_add(omenu, rb)
     
     menu_add(hmenu, "Help...", (path)->println("open help file"))
     menu_add(hmenu, Separator(w))
     menu_add(hmenu, "About", (path)->Messagebox(w, title="Author", message="Xia Lixun"))
 
 
-    # notebook
+
+    #
+    ##
     nb = Notebook(w)
     pack(nb, expand=true, fill="both")
 
-    # frames
-    page1 = Frame(nb)
-    page_add(page1, "Soundcard")
-    
-
-    lf3 = Labelframe(page1, "Soundcard Information")
-    pack(lf3, expand=true, fill="both", side="bottom")
-    snd_stat = Label(lf3, "[asio]: no device found")
-    grid(snd_stat,1,1,sticky="news")
-    if !isempty("SoundcardAPI.deviceCnt")
-        snd_stat[:text] = "returned values" 
+    function newtasktab(path)
+        tasktab = Frame(nb)
+        page_add(tasktab, newtab_entry())
+        tasktab
     end
+    menu_add(omenu, "New Test Item...", newtasktab)
 
-
-
-    lf1 = Labelframe(page1, "Microphone/Input Mixing Matrix")
-    pack(lf1, expand=true, fill="both", side="left")
-
-    grid(Label(lf1,"PORT [1] ⇨"),3,1)
-    grid(Label(lf1,"PORT [2] ⇨"),4,1)
-    grid(Label(lf1,"PORT [3] ⇨"),5,1)
-    grid(Label(lf1,"PORT [4] ⇨"),6,1)
-
-    grid(Label(lf1,"[1]"),1,2)
-    grid(Label(lf1,"[2]"),1,3)
-    grid(Label(lf1,"[3]"),1,4)
-    grid(Label(lf1,"⇧"),2,2)
-    grid(Label(lf1,"⇧"),2,3)
-    grid(Label(lf1,"⇧"),2,4)
-
-    e_mmm = Array{Any}(14,13)
-    for i = 3:16, j = 2:14
-        e_mmm[i-2,j-1] = Entry(lf1, width=4)
-        grid(e_mmm[i-2,j-1],i,j, sticky="news")
-        set_value(e_mmm[i-2,j-1],"0.0")
-    end
-    
-
-
-    lf2 = Labelframe(page1, "Loudspeaker/Output Mixing Matrix")
-    pack(lf2, expand=true, fill="both")
-
-    grid(Label(lf2,"PORT [1] ⇽"),2,1)
-    grid(Label(lf2,"PORT [2] ⇽"),3,1)
-    grid(Label(lf2,"PORT [3] ⇽"),4,1)
-    grid(Label(lf2,"PORT [4] ⇽"),5,1)
-
-    grid(Label(lf2,"PLAY [1]"),1,2)
-    grid(Label(lf2,"PLAY [2]"),1,3)
-    grid(Label(lf2,"PLAY [3]"),1,4)
-    grid(Label(lf2,"PLAY [4]"),1,5)
-
-    for i = 2:5, j = 2:5
-        grid(Entry(lf2),i,j)
-    end
 
 
     
+    #
+    # system config 
+    page1 = Tk.Frame(nb)
+    Tk.page_add(page1, "System Configurations")
+    lf1 = Tk.Labelframe(page1, "Software Information")
+    Tk.pack(lf1, expand=true, fill="both")
+
+    e_proj = Tk.Entry(lf1)
+    e_ver = Tk.Entry(lf1)
+    e_rate = Tk.Entry(lf1)
+    e_srv = Tk.Entry(lf1)
+    b_update = Tk.Button(lf1, "Update")
+
+    Tk.formlayout(e_proj, "Project Name ")
+    Tk.formlayout(e_ver, "Tool Version ")
+    Tk.formlayout(e_rate, "Sample Rate ")
+    Tk.formlayout(e_srv, "Score Server IP ")
+    Tk.formlayout(b_update, nothing)
+    Tk.focus(e_proj)
+
+    
+    ##
+    lf2 = Tk.Labelframe(page1, "Reference Microphone")
+    Tk.pack(lf2, expand=true, fill="both")
+
+    e_rfport = Tk.Entry(lf2)
+    e_rflevcal = Tk.Entry(lf2)
+    b_rflevcal = Tk.Button(lf2, "Browse...")
+    Tk.formlayout(e_rfport, "Port Assignment ")
+    Tk.formlayout(e_rflevcal, "Level Calibration ")
+    Tk.formlayout(b_rflevcal, nothing)
+
+    function callback_update_rfmic(path)
+        conf["Reference Mic"]["Level Calibration"] = Tk.ChooseDirectory()
+        Tk.set_value(e_rflevcal, conf["Reference Mic"]["Level Calibration"])
+    end
+    Tk.bind(b_rflevcal, "command", callback_update_rfmic)
+    # snd_stat = Label(lf3, "[asio]: no device found")
+    # grid(snd_stat,1,1,sticky="news")
+    # if !isempty("SoundcardAPI.deviceCnt")
+    #     snd_stat[:text] = "returned values" 
+    # end
+    function callback_update_sw(path)
+        if haskey(conf, "Project")
+            Tk.set_value(e_proj, conf["Project"])
+        else
+            Tk.set_value(e_proj, "")
+        end
+        if haskey(conf, "Version")
+            Tk.set_value(e_ver, conf["Version"])
+        else
+            Tk.set_value(e_ver, "")
+        end
+        if haskey(conf, "Sample Rate")
+            Tk.set_value(e_rate, string(conf["Sample Rate"]))
+        else
+            Tk.set_value(e_rate, "")
+        end
+        if haskey(conf, "Score Server IP")
+            Tk.set_value(e_srv, conf["Score Server IP"])
+        else
+            Tk.set_value(e_srv, "")
+        end
+
+        if haskey(conf, "Reference Mic")
+            Tk.set_value(e_rfport, string(conf["Reference Mic"]["Port"]))
+            Tk.set_value(e_rflevcal, string(conf["Reference Mic"]["Level Calibration"]))
+        else
+            Tk.set_value(e_rfport, "")
+            Tk.set_value(e_rflevcal, "")
+        end
+        # msg = "You have a nice name $val"
+        # Messagebox(w,  msg)
+    end  
+    Tk.bind(b_update, "command", callback_update_sw)
+
 
 
 
@@ -168,4 +220,21 @@ function ui_tk()
     #     Messagebox(w, title="OK", message="good")
     # end
     # bind(page1_b, "command", callback)
+end
+
+
+function newtab_entry()
+
+    w = Tk.Toplevel("New Test Item")
+    f = Tk.Frame(w)
+    Tk.pack(f, expand=true, fill="both")
+    
+    e = Tk.Entry(f)
+    Tk.formlayout(e, "Title(Test Item ID) ")
+    Tk.focus(e)			## put keyboard focus on widget
+
+    Tk.Messagebox(title="Action", message="Please enter the title...")
+    val = Tk.get_value(e)
+    Tk.destroy(w)
+    val    
 end
