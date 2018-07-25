f8cec890bcea728a1285e361fefb65f5a2df24ea
lixun.xia <lixun.xia@yahoo.com>
2018-07-25 10:19:02 +0800

    fixed a bug on byte-order

--- Test_Automation/Matlab/raw2wav_16bit.m
+++ Test_Automation/Matlab/raw2wav_16bit.m
@@ -13,19 +13,3 @@ end
 
 
 
-function raw2wav_24bit(raw_file, channels, fs, wav_file)
-
-    fid = fopen(raw_file, 'r');
-    x = fread(fid, inf, 'uint8');
-    n = length(x);
-    
-    u = int32(zeros(n/3,1));
-    y = [reshape(x, 3, n/3); zeros(1, n/3)];
-    for i = 1:n/3
-        u(i) = typecast(y(:,i), 'int32');
-    end
-
-    m = reshape(u, channels, n/3/channels);
-    audiowrite(wav_file, m.', fs, 'BitsPerSample', 32);
-    fclose(fid);
-end
